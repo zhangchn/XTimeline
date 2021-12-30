7169bfae440053b7cf150534927e6687f2645c28
Zhang Chen <tgfbeta@me.com>
2021-12-30 16:29:19 +0800

    refactor

---
+++ XTimeline/Loaders/Reddit/RedditData.swift
@@ -0,0 +1,86 @@
+//
+//  RedditData.swift
+//  XTimeline
+//
+//  Created by ZhangChen on 2021/12/30.
+//  Copyright © 2021 ZhangChen. All rights reserved.
+//
+
+import Foundation
+
+struct SubredditPage : Codable {
+    let kind: String
+    struct Media: Codable {
+        struct Embed: Codable {
+            let providerUrl: String?
+            let description: String?
+            let title: String?
+            let thumbnailUrl: String?
+            let html: String?
+            let type: String?
+        }
+        
+        let type: String?
+        let oembed: Embed?
+        let redditVideo: VideoPreview?
+    }
+    struct MediaMetaDataItem: Codable {
+        struct SourceItem: Codable {
+            let x: Int
+            let y: Int
+            let u: String?
+            let mp4: String?
+            let gif: String?
+        }
+        let status: String
+        let m: String? // MIME
+        let s: SourceItem?
+    }
+    struct VideoPreview: Codable {
+        let fallbackUrl: String?
+        let scrubberMediaUrl: String?
+        let hlsUrl: String?
+        let duration: Int?
+    }
+    struct Preview: Codable {
+        struct Image: Codable {
+            struct Source: Codable {
+                let url: String?
+                let width: Int
+                let height: Int
+            }
+            let source: Source?
+            let resolutions: [Source]?
+            
+        }
+        let images: [Preview.Image]?
+        let redditVideoPreview: VideoPreview?
+    }
+    struct Child: Codable {
+        let kind: String
+        struct ChildData: Codable {
+            
+            let author: String?
+            let title: String?
+            let selftext: String?
+            let preview: Preview?
+            let url: String?
+            let permalink: String?
+            let domain: String?
+            let isRedditMediaDomain: Bool?
+            let isSelf: Bool?
+            let media: Media?
+            let mediaMetadata: [String: MediaMetaDataItem]?
+            let id: String?
+            let createdUtc: Int?
+        }
+        let data: ChildData
+    }
+    struct DataObj: Codable {
+        let children: [Child]
+        let after: String?
+        let before: String?
+    }
+    let data: DataObj
+    let url: String?
+}
--- XTimeline/Loaders/Reddit/RedditLoader.swift
+++ XTimeline/Loaders/Reddit/RedditLoader.swift
@@ -13,7 +13,6 @@ import UIKit
 import MobileCoreServices
 #endif
 import AVFoundation
-import SQLite3
 
 func generateThumbnail(for url: URL, cacheFileUrl: URL, attributes:[String: Any] , completion: @escaping ([LoadableImageEntity])->()) {
     let asset = AVAsset(url: cacheFileUrl)
@@ -38,7 +37,7 @@ func generateThumbnail(for url: URL, cacheFileUrl: URL, attributes:[String: Any]
     })
 
 }
-typealias ChildData = RedditLoader.SubredditPage.Child.ChildData
+typealias ChildData = SubredditPage.Child.ChildData
 fileprivate func previewSize(for child: ChildData) -> CGSize? {
     if let source = child.preview?.images?.first?.source {
         return CGSize(width: source.width, height: source.height)
@@ -145,7 +144,7 @@ fileprivate func children(from json: Data) -> ([ChildData], String?) {
     let decoder = JSONDecoder()
     decoder.keyDecodingStrategy = .convertFromSnakeCase
     do {
-        let doc = try decoder.decode(RedditLoader.SubredditPage.self, from: json)
+        let doc = try decoder.decode(SubredditPage.self, from: json)
         return (doc.data.children.map { $0.data }, doc.data.after)
         
     } catch let err {
@@ -157,199 +156,7 @@ fileprivate func children(from json: Data) -> ([ChildData], String?) {
 
 
 class RedditLoader: AbstractImageLoader {
-    class DBWrapper {
-        typealias DBHandle = OpaquePointer?
-        typealias Statement = OpaquePointer?
 
-        var dbHandle: DBHandle = DBHandle(nilLiteral: ())
-        var q : DispatchQueue = DispatchQueue(label: "dbq")
-        
-        var queryStmt1 = Statement(nilLiteral: ())
-        var queryStmt2 = Statement(nilLiteral: ())
-        var saveStmt = Statement(nilLiteral: ())
-
-        init(external: Bool) throws {
-            //self.subreddit = subreddit
-            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!.appending("/reddit/")
-            let dbFilename = (external ? path + ".external/" : path) + "/cache.db"
-            try dbFilename.withCString { (pFilename) in
-                guard sqlite3_open(pFilename, &dbHandle) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed opening cache.db"])
-                }
-            }
-            
-            let initSql = "CREATE TABLE IF NOT EXISTS rdt_child_data (url text, hash text, sub text, post_id text UNIQUE);"
-            
-            try initSql.withCString { (initCStr) in
-                var statement = Statement(nilLiteral: ())
-                guard sqlite3_prepare_v2(dbHandle, initCStr, Int32(initSql.lengthOfBytes(using: .utf8)), &statement, nil) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed prepare initSql"])
-                }
-                guard sqlite3_step(statement) == SQLITE_DONE && sqlite3_finalize(statement) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed exec initSql"])
-                }
-            }
-            
-            let indexSql = "CREATE INDEX IF NOT EXISTS rdt_sub_post ON rdt_child_data (sub, post_id);"
-            try indexSql.withCString { (initCStr) in
-                var statement = Statement(nilLiteral: ())
-                guard sqlite3_prepare_v2(dbHandle, initCStr, Int32(indexSql.lengthOfBytes(using: .utf8)), &statement, nil) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed prepare indexSql"])
-                }
-                guard sqlite3_step(statement) == SQLITE_DONE && sqlite3_finalize(statement) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed exec indexSql"])
-                }
-            }
-            
-            let query1 = "SELECT hash FROM rdt_child_data WHERE post_id = ? AND sub = ?;"
-            try query1.withCString({ (cstr) in
-                guard sqlite3_prepare_v2(dbHandle, cstr, Int32(query1.lengthOfBytes(using: .utf8)), &self.queryStmt1, nil) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed prepare query1"])
-                }
-            })
-            let query2 = "SELECT hash FROM rdt_child_data WHERE url = ?;"
-            try query2.withCString({ (cstr) in
-                guard sqlite3_prepare_v2(dbHandle, cstr, Int32(query2.lengthOfBytes(using: .utf8)), &self.queryStmt2, nil) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed prepare query2"])
-                }
-            })
-            
-            let save = "INSERT OR REPLACE INTO rdt_child_data (sub, url, post_id, hash) VALUES (?, ?, ?, ?);"
-            try save.withCString({ (cstr) in
-                guard sqlite3_prepare_v2(dbHandle, cstr, Int32(save.lengthOfBytes(using: .utf8)), &self.saveStmt, nil) == SQLITE_OK else {
-                    throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed prepare savestmt"])
-                }
-            })
-        }
-        func query(sub: String, post: String, completion: @escaping (String?) -> () ) {
-            q.async {
-                let result = post.withCString { (postStr) -> String? in
-                    sub.withCString { (subStr) -> String? in
-                        guard sqlite3_bind_text(self.queryStmt1, 1, subStr, Int32(sub.utf8.count), nil) == SQLITE_OK
-                            && sqlite3_bind_text(self.queryStmt1, 2, postStr, Int32(post.utf8.count), nil) == SQLITE_OK else {
-                                return nil
-                        }
-                        switch sqlite3_step(self.queryStmt1) {
-                        case SQLITE_ROW:
-                            return String(cString: sqlite3_column_text(self.queryStmt1, 0))
-                        default:
-                            break
-                        }
-                        return nil
-                    }
-                }
-                sqlite3_reset(self.queryStmt1)
-                completion(result)
-            }
-        }
-        func query(url: String, completion: @escaping (String?) -> () ) {
-            q.async {
-                let result = url.withCString { (urlStr) -> String? in
-                    guard sqlite3_bind_text(self.queryStmt2, 1, urlStr, Int32(url.utf8.count), nil) == SQLITE_OK else {
-                        return nil
-                    }
-                    switch sqlite3_step(self.queryStmt2) {
-                    case SQLITE_ROW:
-                        return String(cString: sqlite3_column_text(self.queryStmt2, 0))
-                    default:
-                        break
-                    }
-                    return nil
-                }
-                sqlite3_reset(self.queryStmt2)
-                completion(result)
-            }
-        }
-        
-        func queryBatch(sub: String, after: String = "", count: Int) -> [(String, String)] /*[(post_id, hash)]*/ {
-            var result :[(String, String)] = []
-            let query1 = after == "" ? "SELECT post_id, MIN(hash) FROM rdt_child_data WHERE sub = ?1 GROUP BY post_id ORDER BY post_id DESC LIMIT ?2;" : "SELECT post_id, MIN(hash) FROM rdt_child_data WHERE sub = ?1 AND post_id < ?2 GROUP BY post_id ORDER BY post_id DESC LIMIT ?3;"
-            q.sync {
-                try? query1.withCString({ (cstr) in
-                
-                    var stmt = Statement(nilLiteral: ())
-                    guard sqlite3_prepare_v2(dbHandle, cstr, Int32(query1.lengthOfBytes(using: .utf8)), &stmt, nil) == SQLITE_OK else {
-                        throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed preparing query1"])
-                    }
-                    
-                    guard after.withCString({ (afterStr) -> Bool in
-                        guard sub.withCString({(subStr) -> Bool in
-                            let r1 = sqlite3_bind_text(stmt, 1, subStr, Int32(strlen(subStr)), nil)
-                            if after == "" {
-//                                let r2 = sqlite3_bind_text(stmt, 2, afterStr, Int32(strlen(afterStr)), nil)
-                                let r3 = sqlite3_bind_int(stmt, 2, Int32(count))
-                                guard  (r1 == SQLITE_OK) && (r3 == SQLITE_OK) else {
-                                    return false
-                                }
-                            } else {
-                                let r2 = sqlite3_bind_text(stmt, 2, afterStr, Int32(strlen(afterStr)), nil)
-                                let r3 = sqlite3_bind_int(stmt, 3, Int32(count))
-                                guard  (r1 == SQLITE_OK) && (r2 == SQLITE_OK) && (r3 == SQLITE_OK) else {
-                                    return false
-                                }
-                            }
-                            
-                            var r : Int32
-                            r = sqlite3_step(stmt)
-                            while (r == SQLITE_ROW) {
-                                if let pIdStr = sqlite3_column_text(stmt, 0),
-                                    let hashStr = sqlite3_column_text(stmt, 1) {
-                                    let pId = String(cString: pIdStr)
-                                    let hash = String(cString: hashStr)
-                                    result.append((pId, hash))
-                                }
-                                r = sqlite3_step(stmt)
-                            }
-                            
-                            return true
-                        }) else {return false}
-                        return true
-                    }) else {
-                        throw NSError(domain: "DBWrapper", code: 1, userInfo: [NSLocalizedFailureReasonErrorKey: "Failed binding query1"])
-                    }
-                    _ = sqlite3_finalize(stmt)
-                })
-            }
-            return result
-        }
-        func save(sub: String, url: String, postId: String, hash: String) {
-            q.async {
-                
-                url.withCString { (urlStr) in
-                    postId.withCString { (postStr) in
-                        sub.withCString { (subStr) in
-                            hash.withCString { (hashStr) in
-                                guard sqlite3_bind_text(self.saveStmt, 1, subStr, Int32(sub.utf8.count), nil) == SQLITE_OK else {
-                                    return
-                                }
-                                guard sqlite3_bind_text(self.saveStmt, 2, urlStr, Int32(url.utf8.count), nil) == SQLITE_OK else {
-                                    return
-                                }
-                                guard sqlite3_bind_text(self.saveStmt, 3, postStr, Int32(postId.utf8.count), nil) == SQLITE_OK else {
-                                    return
-                                }
-                                guard sqlite3_bind_text(self.saveStmt, 4, hashStr, Int32(hash.utf8.count), nil) == SQLITE_OK else {
-                                    return
-                                }
-//                                guard sqlite3_bind_text(self.saveStmt, 5, postStr, Int32(postId.utf8.count), nil) == SQLITE_OK else {
-//                                    return
-//                                }
-                                sqlite3_step(self.saveStmt)
-                            }
-                        }
-                    }
-                }
-                sqlite3_reset(self.saveStmt)
-            }
-        }
-        deinit {
-            q.sync {
-                sqlite3_finalize(queryStmt1)
-                sqlite3_finalize(queryStmt2)
-                sqlite3_close(dbHandle)
-            }
-        }
-    }
     static var sharedExternalDB: DBWrapper!
     static var sharedInternalDB: DBWrapper!
     
@@ -417,82 +224,7 @@ class RedditLoader: AbstractImageLoader {
             return nil
         }
     }
-    struct SubredditPage : Codable {
-        let kind: String
-        struct Media: Codable {
-            struct Embed: Codable {
-                let providerUrl: String?
-                let description: String?
-                let title: String?
-                let thumbnailUrl: String?
-                let html: String?
-                let type: String?
-            }
-            
-            let type: String?
-            let oembed: Embed?
-            let redditVideo: VideoPreview?
-        }
-        struct MediaMetaDataItem: Codable {
-            struct SourceItem: Codable {
-                let x: Int
-                let y: Int
-                let u: String?
-                let mp4: String?
-                let gif: String?
-            }
-            let status: String
-            let m: String? // MIME
-            let s: SourceItem?
-        }
-        struct VideoPreview: Codable {
-            let fallbackUrl: String?
-            let scrubberMediaUrl: String?
-            let hlsUrl: String?
-            let duration: Int?
-        }
-        struct Preview: Codable {
-            struct Image: Codable {
-                struct Source: Codable {
-                    let url: String?
-                    let width: Int
-                    let height: Int
-                }
-                let source: Source?
-                let resolutions: [Source]?
-                
-            }
-            let images: [Preview.Image]?
-            let redditVideoPreview: VideoPreview?
-        }
-        struct Child: Codable {
-            let kind: String
-            struct ChildData: Codable {
-                
-                let author: String?
-                let title: String?
-                let selftext: String?
-                let preview: Preview?
-                let url: String?
-                let permalink: String?
-                let domain: String?
-                let isRedditMediaDomain: Bool?
-                let isSelf: Bool?
-                let media: Media?
-                let mediaMetadata: [String: MediaMetaDataItem]?
-                let id: String?
-                let createdUtc: Int?
-            }
-            let data: ChildData
-        }
-        struct DataObj: Codable {
-            let children: [Child]
-            let after: String?
-            let before: String?
-        }
-        let data: DataObj
-        let url: String?
-    }
+
     override func loadNextBatch(with url: URL, completion: @escaping ([EntityKind]) -> ()) {
         let downPath = self.cachePath + ".json"
         let saveCache = fileManager.fileExists(atPath: downPath)
